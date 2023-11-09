1c34c47a9a1cb314045434ee15c5fa7ed6a9042b
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-11-09 20:52:46 +0100

    Animations

--- app/src/main/java/otus/gpb/studioapp/MainActivity.kt
+++ app/src/main/java/otus/gpb/studioapp/MainActivity.kt
@@ -1,14 +1,20 @@
 package otus.gpb.studioapp
 
+import android.animation.ObjectAnimator
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.view.animation.BounceInterpolator
+import android.widget.Button
 import android.widget.TextView
 import otus.gpb.mylibrary.Printer
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var text: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +25,17 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        findViewById<TextView>(R.id.text).text = Printer.print()
+        text = findViewById(R.id.text)
+        text.text = Printer.print()
+        val animation = ObjectAnimator.ofFloat(text, View.SCALE_X, 1.0f, 0.0f).apply {
+            duration = 2000
+            repeatCount = 3
+            repeatMode = ObjectAnimator.REVERSE
+            interpolator = BounceInterpolator()
+        }
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            animation.start()
+        }
     }
 }
